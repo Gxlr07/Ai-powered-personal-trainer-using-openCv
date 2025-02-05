1b098b13fe662ce2cb1a8928827a86fd84286473
Gxlr07 <227r1a7394@cmrtc.ac.in>
2025-02-05 10:48:24 +0530

    Delete workout_plan.py

--- workout_plan.py
+++
@@ -1,7 +0,0 @@
-def generate_workout_plan(user_data):
-    if user_data['goal'] == 'weight_loss':
-        return ["Jumping Jacks", "Burpees", "Mountain Climbers", "Running"]
-    elif user_data['goal'] == 'strength':
-        return ["Deadlifts", "Squats", "Pull-Ups", "Push-Ups"]
-    else:
-        return ["Basic Cardio", "Stretching"]
